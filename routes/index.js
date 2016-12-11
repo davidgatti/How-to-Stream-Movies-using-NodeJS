5d4b4dd9889f58fcb6aca08f8ddc41de41f2345a
David Gatti <office@gatti.pl>
2016-12-11 14:22:31 +0100

    Added base url

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,9 @@ router.get('/', function(req, res, next) {
 	//
 	//	->	Display the index view with the video tag
 	//
-	res.render("index");
+	res.render("index", {
+		base_url: process.env.BASE_URL
+	});
 
 });
 
